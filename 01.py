9e395b8280169cfbb669b0f4c4659120a081a10d
kroberts <cppwriter@yahoo.com>
2020-12-13 21:14:16 -0800

    01 part 2

--- 01.py
+++ 01.py
@@ -9,3 +9,10 @@ with open("01.txt") as f:
             print('part1:', i, alt_i, i * alt_i)
             break
 
+    for i in nums:
+        for j in nums:
+            alt_ij = 2020 - i - j
+            if alt_ij in nums:
+                print('part2:', i, j, alt_ij, i * j * alt_ij)
+                quit()
+
