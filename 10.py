e8eef62812ddab1853070628b08e44e619b81cda
kroberts <cppwriter@yahoo.com>
2020-12-22 21:01:41 -0800

    10 part 2 in progress

--- 10.py
+++ 10.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 
 def recurse(adapters, cur_jolts, count1, count3):
     #print('start', cur_jolts, adapters)
@@ -20,11 +21,50 @@ def recurse(adapters, cur_jolts, count1, count3):
     #print('ret', cur_jolts, adapters)
     return None
 
+def recurse2(adapters, cur_jolts, exp, depth):
+    print(' ' * depth, 'start', cur_jolts)
+    if cur_jolts + 3 == exp:
+        #print('found!')
+        return 1
+    tot = 0
+    for (i, a) in enumerate(adapters):
+        if a <= cur_jolts + 3 and a >= cur_jolts + 1:
+            #print('loop', a)
+            new_adapters = adapters[i:]
+            #print('recursing cj=', cur_jolts, 'a=', a, new_adapters)
+            tot += recurse2(new_adapters, a, exp, depth + 1)
+
+    if cur_jolts == 0:
+        print(tot)
+    return tot
+
+def breadth2(adapters, cur_jolts, exp, depth):
+    q = [cur_jolts]
+
+    count = 0
+    while len(q) > 0:
+        c = q.pop()
+        if c + 3 == exp:
+            count += 1
+            if count % 1000000 == 0:
+                print(datetime.now(), count, len(q))
+            continue
+        for i in range(1, 4):
+            if c + i in adapters:
+                q.append(c + i)
+
+    return count
+
+
 with open("10.txt") as f:
     adapters = set(int(x) for x in f)
 
 #print(adapters)
 
 ret = recurse(adapters.copy(), 0, 0, 0)
-print(ret)
 print('part1', ret[0] * ret[1])
+
+exp = max(adapters) + 3
+print(exp)
+tot = breadth2(adapters, 0, exp, 0)
+print('part2', tot)
