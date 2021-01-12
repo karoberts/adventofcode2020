6486db6e5b1c6d7fd5c256954638e659f55708b3
kroberts <cppwriter@yahoo.com>
2021-01-12 12:57:52 -0800

    tweaks so 25 runs faster

--- 25.py
+++ 25.py
@@ -18,12 +18,10 @@ def xform(subj, ls, memo = None):
     i_start = 0
     n = subj
     if memo is not None:
-        for i in range(ls, 0, -1):
-            #tot_loops += 1
-            if i in memo:
-                n = memo[i]
-                i_start = i
-                break
+        if ls not in memo:
+            memo[ls] = pow(subj, ls + 1, 20201227)
+        i_start = ls
+        n = memo[ls]
 
     for i in range(i_start, ls):
         n *= subj
@@ -34,20 +32,24 @@ def xform(subj, ls, memo = None):
 
     return n
 
-def find_ls(pk, subj, memo=None):
-    for loop_size in range(0, 20_000_000):
+def find_ls(pk, subj, memo=None, sthint=0):
+    for loop_size in range(sthint, 20_000_000):
         """
         if loop_size % 1_000_000 == 0:
             print(loop_size, tot_loops)
         """
         n = xform(subj, loop_size, memo)
+        # this works too, but is slower
+        #n = pow(subj, loop_size + 1, 20201227)
         #print(f'ls = {loop_size}, xform = {n}')
         if n == pk:
             return loop_size
     raise Exception(f'not found for {pk}')
 
-card_ls = find_ls(card, 7, memo_card)
-door_ls = find_ls(door, 7, memo_door)
+memo_card[17_100_000] = pow(7, 17_100_001, 20201227)
+memo_door[19_800_000] = pow(7, 19_800_001, 20201227)
+card_ls = find_ls(card, 7, memo_card, 17_100_000)
+door_ls = find_ls(door, 7, memo_door, 19_800_000)
 
 print(f'card = {card_ls + 1}, door = {door_ls + 1}')
 
